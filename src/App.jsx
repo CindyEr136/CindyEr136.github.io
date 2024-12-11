817b81a972cf48ee7c7824d098a9131dd968083e
CdEr22 <er.cindy168@gmail.com>
2024-12-11 18:41:35 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ const App = () => {
           }
         />
         <Route
-          path="/BrainyBees"
+          path="BrainyBees/"
           element={
             <div className="relative z-0 bg-primary">
               <NavbarBack />
