ce680b544fa731c7af01c5a06c958c738c4285ab
CdEr22 <er.cindy168@gmail.com>
2024-12-11 17:33:22 -0500

    commit

--- src/components/NavbarBack.jsx
+++ src/components/NavbarBack.jsx
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { styles } from "../styles";
-import closeIcon from "../assets/close.png";
+import closeIcon from "../assets/arrow.png";
 
 const NavbarBack = () => {
   return (
